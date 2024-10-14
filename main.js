c78dc042f59aefe60a7be84e1cf91dd7235739e7
senagab <senagab@gmail.com>
2024-10-14 10:14:35 -0400

    chore: remoção de código comentado e sem utilidade

--- main.js
+++ main.js
@@ -22,25 +22,6 @@ $(document).ready(function () {
         event.preventDefault()
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
-        
-
-        // $.ajax(endpoint).done(function (resposta){
-        //     const logradouro = resposta.logradouro;
-        //     const bairro = resposta.bairro;
-        //     const cidade = resposta.localidade;
-        //     const estado = resposta.uf;
-        //     const endereco = `${logradouro}, ${bairro}, ${cidade}, ${estado}`;
-            
-        //     // console.log(endereco);
-
-        //     $('#endereco').val(endereco);
-
-        //     setTimeout(function () {
-        //         $(botao).find('i').removeClass('d-none');
-        //         $(botao).find('span').addClass('d-none');
-        //     }, 2000);
-
-        // })
 
         fetch(endpoint).then(function (resposta) {
             // console.log(resposta)
